861767f3b6e83a9d9c38ce4b52a3139cb96cd20a
Howard.Zuo <leftstick@qq.com>
2021-06-22 16:24:28 +0800

    update demo

--- test/input.js
+++ test/input.js
@@ -1,4 +1,14 @@
+// case 1
 const userA = 'hello'
 const userB = 'world'
 
-console.log(userA ^ userB)
+console.log(userA ^ userB);
+
+// case 2
+console.log(1 ^ 2);
+
+
+// case 3
+const arr = ['a', 'b', 'c']
+
+console.log(arr.filter(el => el ^ 'b'))
