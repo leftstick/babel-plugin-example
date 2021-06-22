ad83849bd0402a0211e1d072db5d2bdb4c5b1f01
Howard.Zuo <leftstick@qq.com>
2021-06-22 16:51:13 +0800

    chore: add comments

--- test/input.js
+++ test/input.js
@@ -1,14 +1,14 @@
-// case 1
+// case 1: variable comparison 
 const userA = 'hello'
 const userB = 'world'
 
 console.log(userA ^ userB);
 
-// case 2
+// case 2: value comparison
 console.log(1 ^ 2);
 
 
-// case 3
+// case 3: using in array function(aka: lambda)
 const arr = ['a', 'b', 'c']
 
 console.log(arr.filter(el => el ^ 'b'))
